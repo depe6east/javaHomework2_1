efbb80648b23a09c043ee53846613182a36f3676
Andrey Bagaev <bagaev_a@mail.ru>
2023-07-17 21:46:12 +0400

    finalScore

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// Press Shift twice to opgien the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        int score = 100;
+        int replenish = 1100;
+        int bonus = 0;
+        if (replenish > 1000) {
+            bonus = replenish / 100;
         }
+        int finalScore = score + replenish + bonus;
+        System.out.println("Итоговый счет = " + finalScore);
+
     }
 }
